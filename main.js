e66bd61fe30cf00f4cfbabfa831d049306058fb6
Ehuié <ehuie.manvoule@etudiant.univ-rennes1.fr>
2021-02-18 15:50:46 +0100

    Implémentation du modele: Drawing, Form, Rectangle, Line

--- main.js
+++ main.js
@@ -12,11 +12,17 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
 /////
 
 // Code temporaire pour tester l'affiche de la vue
-//var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
 //rec.paint(ctx);
-//var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+var ligne = new Line(10, 20, 50, 100, 5, '#00ccc0');
 //ligne.paint(ctx);
 // tester également Dessin.
+
+var drawing = new Drawing();
+
+drawing.setForms(ligne)
+drawing.setForms(rec)
+drawing.paint(ctx)
 ////
 
 // Code final à utiliser pour manipuler Pencil.
--- model.js
+++ model.js
@@ -1,3 +1,116 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+/**
+ * Drawing
+ * @param forms
+ * @constructor
+ */
+function Drawing() {
+    this.forms = new Array()
+}
+Drawing.prototype.setForms = function (forms = Array()) {
+    this.forms.push(forms)
+}
+
+Drawing.prototype.getForms = function () {
+    return this.forms
+}
+
+Drawing.prototype.paint = function(ctx) {
+    console.log(this.getForms());
+
+    this.getForms().forEach(function (eltDuTableau) {
+        // now fill the canvas
+        ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        console.log(eltDuTableau);
+        eltDuTableau.paint(ctx);
+    });
+};
+
+/**
+ * Form
+ * @param lineH
+ * @param color
+ * @constructor
+ */
+function Form(lineH, color) {
+    this.lineHeigth = lineH
+    this.color = color
+}
+
+Form.prototype.paint = function (ctx) {
+
+}
+
+/**
+ * Rectangle
+ * @param topLeftX
+ * @param topLeftY
+ * @param l
+ * @param h
+ * @param lineH
+ * @param color
+ * @constructor
+ */
+function Rectangle(topLeftX, topLeftY, l, h, lineH, color) {
+    Form.call(lineH, color)
+    this.intiX = topLeftX
+    this.intiY = topLeftY
+    this.finalX = l
+    this.finalY = h
+}
+Rectangle.prototype = new Form()
+
+Rectangle.prototype.getInitX = function() {
+    return this.intiX
+}
+Rectangle.prototype.getInitY = function() {
+    return this.intiY
+}
+Rectangle.prototype.getFinalX = function() {
+    return this.finalX
+}
+Rectangle.prototype.getFinalY = function() {
+    return this.finalY
+}
+
+Rectangle.prototype.paint = function(ctx) {
+    //TODO Manager color
+    Form.prototype.paint.call(ctx)
+    ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+};
+
+
+/**
+ * Line
+ * @param beginX
+ * @param beginY
+ * @param endX
+ * @param endY
+ * @param lineH
+ * @param color
+ * @constructor
+ */
+function Line(beginX, beginY, endX, endY, lineH, color) {
+    Form.call(lineH, color)
+    this.intiX = beginX
+    this.intiY = beginY
+    this.finalX = endX
+    this.finalY = endY
+}
+
+Line.prototype = new Rectangle()
+
+Line.prototype.paint = function(ctx) {
+    //TODO Manager color
+    Form.prototype.paint.call(ctx)
+    ctx.beginPath();
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+};
+
