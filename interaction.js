00178f635e10f6b55eabec08995bf6a4f0ac92d4
Ehuié <ehuie.manvoule@etudiant.univ-rennes1.fr>
2021-02-18 14:28:33 +0100

    Implémentation des evts du DnD

--- interaction.js
+++ interaction.js
@@ -2,11 +2,55 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  // Définir ici les attributs de la 'classe'
+  var initial_x = 0
+  var initial_y = 0
+  var final_x = 0
+  var final_y = 0
+  var hasPress = false
 
-	// Developper les 3 fonctions gérant les événements
+  // Developper les 3 fonctions gérant les événements
+  DnD.prototype.pression = function (e) {
+    hasPress = true
+    var pos = getMousePosition(canvas, e)
+    console.log("pression")
+    console.log(pos)
+    initial_x = pos.x
+    initial_y = pos.y
+  }
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+  DnD.prototype.deplacement = function (e) {
+    if(hasPress) {
+      pos = getMousePosition(canvas, e)
+      console.log("deplacement")
+      console.log(initial_x)
+      console.log(initial_y)
+      console.log(pos)
+      final_x = pos.x
+      final_y = pos.y
+    }
+  }
+
+  DnD.prototype.relachement = function (e) {
+    if(hasPress) {
+      console.log("relachement")
+      console.log(initial_x)
+      console.log(initial_y)
+      console.log(final_y)
+      console.log(final_y)
+      initial_x = 0
+      initial_y = 0
+      final_x = 0
+      final_y = 0
+      hasPress = false
+    }
+  }
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.pression, false);
+  canvas.addEventListener('mousemove', this.deplacement, false);
+  canvas.addEventListener('mouseup', this.relachement, false);
+
 };
 
 
